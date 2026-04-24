4d34c7a8e184466281aac4b6e6a1677a9838196d
KlrKevin <kewinn.guitar@gmail.com>
2026-04-24 18:59:47 +0200

    added a save and load chat option with json file

--- AI.py
+++ AI.py
@@ -1,11 +1,13 @@
 import os
 from openai import OpenAI
+import json
 
 client = OpenAI(api_key=os.environ.get("OPEN_AI_KEY"))
 
 # list to keep the message history
 messages = []
 
+# dictionary of modes the user can choose from
 modes = {
     "1": "You are a helpful general assistant.",
     "2": "You summarize text clearly and concisely.",
@@ -13,16 +15,24 @@ modes = {
     "4": "You are a coding assistant that explains code simply.",
 }
 
+# loads the previous request in form of dictionaries from json file and appends to the messages list
+if input("do you want to load previous conversation?(y/n)").lower() == "y":
+    with open("data/conversation.json", "r") as f:
+        messages = json.load(f)
+
+
+for key in modes:
+    print(f"{key}. {modes[key]}")
+
+mode_choice = input("choose your mode (1-4) ")
+
 while True:
-    for key in modes:
-        print(f"{key}. {modes[key]}")
-
-    mode_choice = input("choose your mode (1-4) ")
-
+    # input check
     if mode_choice not in modes.keys():
         print("please enter a number between 1-4")
         continue
 
+    # the chosen mode influences the behaviour of the ai
     messages.append({"role": "system", "content": modes[mode_choice]})
 
     user_input = input("You:")
@@ -30,6 +40,11 @@ while True:
     # functionality to exit the loop
     if user_input.lower() == "exit":
         break
+    elif user_input.lower() == "/mode":
+        for key in modes:
+            print(f"{key}. {modes[key]}")
+
+        mode_choice = input("choose your mode (1-4) ")
 
     # addsthe message we send to the api to a list, the list contains dictionaries as the api uses dictionaries as the message
     messages.append({"role": "user", "content": user_input})
@@ -43,3 +58,8 @@ while True:
     messages.append({"role": "assistant", "content": reply})
 
     print("AI:", reply)
+
+# saves the messages list that stores the request in the form of dictionaries sent to the api
+if input("Do you want to save this conversations?(y/n)").lower() == "y":
+    with open("data/conversation.json", "w") as f:
+        json.dump(messages, f)
